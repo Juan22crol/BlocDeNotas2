aa86bbcdd75acace327fa940166701c5a6b66c85
Juan22crol <juanalfonsojuarezz@gmail.com>
2023-06-21 11:15:40 +0200

    first commit

---
+++ src/tareatema16/TareaTema16.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareatema16;
+
+/**
+ *
+ * @author Juan
+ */
+public class TareaTema16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
